b8dbd2b6e8025eb835d0cd0ccfc1a3d1707636fc
Amit Kumar <69058245+amit1820@users.noreply.github.com>
2026-02-08 15:12:30 +0100

    Update 1_Industry_Case_Studies.py

--- pages/1_Industry_Case_Studies.py
+++ pages/1_Industry_Case_Studies.py
@@ -2,6 +2,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Industry Case Studies", layout="wide", page_icon="💼")
+
 # Custom CSS - Same as Home page
 st.markdown("""
 <style>
@@ -122,46 +123,6 @@ a:hover {
 }
 </style>
 """, unsafe_allow_html=True)
-
-# Top Navigation - centered
-nav_cols = st.columns(5)
-with nav_cols[0]:
-    st.markdown("**Home**")
-with nav_cols[1]:
-    if st.button("Industry Cases", key="nav1", use_container_width=True):
-        st.switch_page("pages/1_Industry_Case_Studies.py")
-with nav_cols[2]:
-    if st.button("Projects", key="nav2", use_container_width=True):
-        st.switch_page("pages/2_Personal_Projects.py")
-with nav_cols[3]:
-    if st.button("Research", key="nav3", use_container_width=True):
-        st.switch_page("pages/3_Research.py")
-with nav_cols[4]:
-    if st.button("Contact", key="nav4", use_container_width=True):
-        st.switch_page("pages/4_Contact.py")
-
-st.markdown("<div style='margin: 1rem 0;'></div>", unsafe_allow_html=True)
-
-# Top Navigation - centered
-nav_cols = st.columns(5)
-with nav_cols[0]:
-    if st.button("Home", key="nav0", use_container_width=True):
-        st.switch_page("Home.py")
-with nav_cols[1]:
-    if st.button("Industry Cases", key="nav1", use_container_width=True):
-        st.switch_page("pages/1_Industry_Case_Studies.py")
-with nav_cols[2]:
-    if st.button("Projects", key="nav2", use_container_width=True):
-        st.switch_page("pages/2_Personal_Projects.py")
-with nav_cols[3]:
-    if st.button("Research", key="nav3", use_container_width=True):
-        st.switch_page("pages/3_Research.py")
-with nav_cols[4]:
-    if st.button("Contact", key="nav4", use_container_width=True):
-        st.switch_page("pages/4_Contact.py")
-
-st.markdown("<div style='margin: 1rem 0;'></div>", unsafe_allow_html=True)
-
 
 # Page Title
 st.title("Industry Case Studies")
